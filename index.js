bd259c1b95611bf77180809afc9db099281c5416
Marek Łabuz <marek@marek-debian8>
2015-10-13 19:13:07 +0200

    wstępna integracja z bazą danych

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var port = process.env.PORT || 5000;
 server.listen(port);
 var io = sio.listen(server);
 
-
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
 
@@ -17,18 +16,144 @@ app.get('/charades', function(req, res) {
 });
 
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://db_user:db_user123@ds033734.mongolab.com:33734/wzorce';
+
+//insertData({name:'abc', usersNum:5});
+//findRooms(increaseUsersNum, {name:"abc"});
+//decreaseUsersNum("abc");
+
+var users = {};
+var rooms = [];
+var numUsers = 0;
+
 io.on('connection', function(socket) {
-	socket.on('mouse down', function(data) {
-		socket.broadcast.emit('mouse down', data);
+	socket.on('update rooms', function() {
+		socket.emit('update rooms', sortRooms());
+	});
+
+	socket.on('welcome', function(user) {
+		socket.name = user.name;
+		socket.room = user.room;
+		socket.join(socket.room);
+		
+
+		//findRooms(increaseUsersNum, {name: socket.room});
+
+
+
+
+		var i = findRoom(socket.room);
+		if(i == -1) {
+			rooms.push({
+				name:		socket.room,
+				numUsers:	1
+			});
+		} else {
+			rooms[i].numUsers += 1;
+		}
+		io.emit('update rooms', sortRooms());
+	
+		users[socket.id] = {
+			name:	socket.name,
+			room:	socket.room
+		};
+		numUsers++;
+		
+		console.log('User ' + socket.name + ' added to room ' + socket.room + 
+			' - numUsers: ' + rooms[findRoom(socket.room)].numUsers);
 	});
 	
-	socket.on('mouse drag', function(data) {
-		socket.broadcast.emit('mouse drag', data);
+	socket.on('mouse down', function(msg) {
+	  	socket.broadcast.to(socket.room).emit('mouse down', msg);
+	});
+	
+
+	socket.on('mouse drag', function(msg) {
+	  	socket.broadcast.to(socket.room).emit('mouse drag', msg);
+	});
+	
+	socket.on('message', function(msg) {
+	  	io.to(socket.room).emit('message', msg);
 	});
 	
 	socket.on('disconnect', function() {
+		var i = findRoom(socket.room);
+		if(i != -1) {
+			--rooms[i].numUsers;
+			console.log('User ' + socket.name + ' removed from room ' + socket.room + ' - numUsers: ' + rooms[i].numUsers);
 		
+			if(rooms[i].numUsers == 0) {
+				rooms.splice(i, 1);
+				io.emit('remove room', socket.room);
+			}
+		}
+		delete users[socket.id];
+		--numUsers;
+		io.emit('update rooms', sortRooms());
 	});
+	
+	function sortRooms() {
+		return rooms.sort(function(a, b) {
+			return b.numUsers - a.numUsers;
+		});
+	}
+	
+	
+	function findRoom(roomName) {
+		for(var i = 0; i < rooms.length; i++) {
+			if(rooms[i].name == roomName) return i;
+		}
+		return -1;
+	}
 });
+
+
+function increaseUsersNum(room) {
+	updateRooms({name: room[0].name}, {$inc: { usersNum: 1}});
+}
+
+function decreaseUsersNum(room) {
+	updateRooms({name: room.name}, {$inc: { usersNum: -1 }});
+}
+
+//$set: modified
+//$inc: { quantity: 1, "metrics.orders": 1 }
+
+function updateRooms(conditions, modification) {
+	MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  db.collection('rooms').updateOne(conditions,
+				modification,
+        {$currentDate: { "lastModified": true }}
+      , function(err, results) {
+  					db.close();
+  			});
+		});
+};
+
+function findRooms(callback, conditions) {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		db.collection('rooms').find(conditions).toArray(function (err, docs) {
+				//console.log(err);
+   			callback(docs);
+		   	db.close();
+		});
+	});
+};
+
+function insertData(data) {
+	MongoClient.connect(url, function(err, db) {
+  	assert.equal(null, err);
+		db.collection('rooms').insertOne(data, function(err, result) {
+			assert.equal(err, null);
+			console.log("Inserted a document into the restaurants collection.");
+	 		db.close();
+		});
+	});
+};
 	
 
--- public/javascript/charades.js
+++ public/javascript/charades.js
@@ -1,41 +1,104 @@
-var color = 'black';
 $(document).ready(function() {
-	$(".color").on('click', function() {
-		color = $(this).attr('id');
-		});
+		$("#myCanvas").hide();
+		$("#tool-belt").hide();
 	});
+	
+	var player = {
+		name:			"Marek",
+		serverId:	0,
+		path:			0,
+		pathProperties:	{
+			color:	'black',
+			size:		1
+		},
+		drawPath: function() {
+			this.path = new Path();
+			this.path.strokeColor = this.pathProperties.color;
+			this.path.strokeWidth = this.pathProperties.size;
+		},
+		addPoint: function(position) {
+			if(this.path) this.path.add(new Point(position.x, position.y));
+			view.update();
+		}
+	};
+	
+	var socket = io();
+	
+	paper.install(window);
+	window.onload = function() {
+		//------------------------------------Paper.js--------------------------------------
+		paper.setup('myCanvas');
+		var tool = new Tool();
+		tool.minDistance = 5;
 		
-var socket = io();
-paper.install(window);
-window.onload = function() {
-	paper.setup('myCanvas');
-	var tool = new Tool();
-	tool.minDistance = 5;
-	var path;
+		socket.emit('update rooms');
+		
+		tool.onMouseDown = function(event) {
+			player.drawPath();
+			socket.emit('mouse down', player.pathProperties);
+		}
 
-	tool.onMouseDown = function(event) {
-		path = new Path();
-		path.strokeColor = color;
-		socket.emit('mouse down', {
-			color: path.strokeColor
+		tool.onMouseDrag = function(event) {
+			player.addPoint(event.point);
+			socket.emit('mouse drag', {
+				x:	event.point.x,
+				y:	event.point.y
+			});
+		}
+		
+		//------------------------------------JQuery-----------------------------------------
+		$("#create").on('click', function() {
+			sendWelcome($("#roomname").val());
 		});
-	}
+		
+		$(".color").on('click', function() {
+			player.pathProperties.color = this.id;
+		});
+		
+		$(".size").on('click', function() {
+			player.pathProperties.size = this.id;
+		});
+		
+		
+		//------------------------------------Socket-----------------------------------------
+		
+		socket.on('mouse down', function(properties) {
+			player.pathProperties = properties;
+			player.drawPath();
+		});
+	
+		socket.on('mouse drag', function(position) {
+			player.addPoint(position);
+		});
+		
+		socket.on('update rooms', function(rooms) {
+			addRoomsToList(rooms);
+		});
 
-	tool.onMouseDrag = function(event) {
-		path.add(event.point);
-		socket.emit('mouse drag', {
-			x: event.point.x,
-			y: event.point.y
+		socket.on('remove room', function(room) {
+			$('#' + room + ".room").remove();
 		});
-	}
-			
-	socket.on('mouse down', function(data) {
-		path = new Path();
-		path.strokeColor = data.color;
-	});
-			
-	socket.on('mouse drag', function(data) {
-		path.add(new Point(data.x, data.y));
-		view.update();
-	});
-}
+
+		function addRoomsToList(rooms) {
+			$("#rooms-list").html('');
+			for(var i = 0; i < rooms.length; i++) {
+				$("#rooms-list").append('<button id="' + rooms[i].name + 
+				'" class="list-group-item room"><span class="room-name">' + rooms[i].name + 
+				'</span><span class="badge">' + rooms[i].numUsers + '</span></button>');
+				$('#' + rooms[i].name).on('click', function() {
+					sendWelcome($(this).attr('id'));
+				});
+			}
+		}
+		
+		function sendWelcome(roomName) {
+			socket.emit('welcome', {
+				name:	"Marek",
+				room:	roomName
+			});
+		
+			$("#myCanvas").show();
+			$("#tool-belt").show();
+			$("#rooms").hide();
+		};
+	};
